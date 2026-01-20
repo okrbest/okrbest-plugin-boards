b1a0ecef1498458f0f293b505c8abde01aabe1fb
hyunjin <hun1846@gmail.com>
2026-01-20 22:20:47 +0900

    chore(webapp): BlockSuite 의존성 및 빌드 설정 업데이트
    - package.json: BlockSuite 패키지 버전 업데이트
    - webpack.common.js: BlockSuite 관련 번들 설정
    - custom.d.ts: 타입 선언 추가
    - main.scss: BlockSuite 스타일 import

--- webapp/src/custom.d.ts
+++ webapp/src/custom.d.ts
@@ -1,9 +1,21 @@
+// Copyright (c) 2020-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
 declare module '*.scss?inline' {
-  const content: string;
-  export default content;
+    const content: string
+    export default content
 }
 
 declare module '*.css?inline' {
-  const content: string;
-  export default content;
+    const content: string
+    export default content
+}
+
+// BlockSuite effects 모듈 타입 선언
+declare module '@blocksuite/blocks/effects' {
+    export function effects(): void
+}
+
+declare module '@blocksuite/presets/effects' {
+    export function effects(): void
 }
