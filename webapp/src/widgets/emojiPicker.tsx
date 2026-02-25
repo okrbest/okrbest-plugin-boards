3d956fbcabe17491a6f7e54657ece106d8dcfa92
sinyebin <dpqls0625@naver.com>
2026-02-25 16:29:27 +0900

    feat: 로딩 상태 및 동적 컴포넌트 로딩으로 이모지 피커 개선 EmojiPicker 컴포넌트에 로딩 상태를 추가하여 이모지 피커가 로드되는 동안 스피너를 표시합니다. 초기 로드 성능 향상을 위해 이모지 피커 컴포넌트의 동적 import를 구현했습니다. 로딩 중 사용자 경험 개선을 위해 로딩 상태와 스피너 스타일을 업데이트했습니다.

--- webapp/src/widgets/emojiPicker.tsx
+++ webapp/src/widgets/emojiPicker.tsx
@@ -1,10 +1,10 @@
 // Copyright (c) 2020-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {FC} from 'react'
+import React, {FC, useState, useEffect, useCallback} from 'react'
 
 import data from '@emoji-mart/data'
-import Picker from '@emoji-mart/react'
+import {init as initEmojiMart} from 'emoji-mart'
 
 import './emojiPicker.scss'
 
@@ -20,16 +20,64 @@ type EmojiSelectData = {
     shortcodes: string
 }
 
-const EmojiPicker: FC<React.PropsWithChildren<Props>> = (props: Props): React.JSX.Element => (
-    <div
-        className='EmojiPicker'
-        onClick={(e) => e.stopPropagation()}
-    >
-        <Picker
-            data={data}
-            onEmojiSelect={(emoji: EmojiSelectData) => props.onSelect(emoji.native)}
-        />
-    </div>
-)
+// Picker 컴포넌트 타입
+type PickerComponent = React.ComponentType<{
+    data: typeof data
+    onEmojiSelect: (emoji: EmojiSelectData) => void
+}>
+
+const EmojiPicker: FC<React.PropsWithChildren<Props>> = (props: Props): React.JSX.Element => {
+    const [Picker, setPicker] = useState<PickerComponent | null>(null)
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        let isMounted = true
+
+        const loadPicker = async () => {
+            // emoji-mart 초기화 보장
+            initEmojiMart({data})
+
+            // Picker 컴포넌트 동적 로드
+            const module = await import('@emoji-mart/react')
+            if (isMounted) {
+                setPicker(() => module.default as PickerComponent)
+                setIsLoading(false)
+            }
+        }
+
+        loadPicker()
+
+        return () => {
+            isMounted = false
+        }
+    }, [])
+
+    const handleEmojiSelect = useCallback((emoji: EmojiSelectData) => {
+        props.onSelect(emoji.native)
+    }, [props.onSelect])
+
+    if (isLoading || !Picker) {
+        return (
+            <div
+                className='EmojiPicker EmojiPicker--loading'
+                onClick={(e) => e.stopPropagation()}
+            >
+                <div className='EmojiPicker__spinner'/>
+            </div>
+        )
+    }
+
+    return (
+        <div
+            className='EmojiPicker'
+            onClick={(e) => e.stopPropagation()}
+        >
+            <Picker
+                data={data}
+                onEmojiSelect={handleEmojiSelect}
+            />
+        </div>
+    )
+}
 
 export default EmojiPicker
